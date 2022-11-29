ce7b1a894cb066227ed227a31aaf08c75664f949
Wabisherie <wabisherie@gmail.com>
2022-11-30 00:00:02 +0300

    index

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if number < 0:
+    print("Last digit of {} is {}" .format(number, last_digit), end =" ")
+if last_digit > 0:
+    print("and is 0")
+if < 6 != 0:
+    print("and is less than 6 and not 0")
