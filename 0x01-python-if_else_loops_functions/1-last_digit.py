184712d675e21fc75e175f39181755657bedc635
Wabisherie <wabisherie@gmail.com>
2022-11-30 09:14:31 +0300

    corr syntax for task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
-    print("Last digit of {} is {}" .format(number, last_digit), end =" ")
+    print("Last digit of {} is {}" .format(number, last_digit), end=" ")
 if last_digit > 0:
     print("and is 0")
 if number < 6 != 0:
